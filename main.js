2902d37d484938831e950dd9376a83e6755da7eb
Abenathi <cwatiabenathi@gmail.com>
2022-08-03 04:10:43 -0700

    main.js edited

--- main.js
+++ main.js
@@ -4,5 +4,14 @@ let inputField = document.getElementById('inputField');
 
 AddToDobutton.addEventListener('click', function(){
     var paragraph = document.createElement('p')
+    paragraph.classList.add('paragraph-styling');
+    paragraph.innerText =inputField.value;
     toDOContainer.appendChild(paragraph);
+    inputField.value = null;
+    paragraph.addEventListener('click', function(){
+        paragraph.style.textDecoration ="line-through";
+    })
+    paragraph.addEventListener('dblclick', function(){
+        toDOContainer.removeChild(paragraph);
+    })
 })
